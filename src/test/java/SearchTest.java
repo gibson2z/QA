2a81edb0e94a1bf29c3434d741fc6f517e5aa640
Alexander Chechulin <bmw.f25@gmail.com>
2025-01-15 12:28:42 +0300

    Add files via upload

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import element.web;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTest {
+
+    @Test
+    void selenideSearchTest() {
+        //Открыть https://google.com
+        open("https://www.google.com/");
+
+        element(byId("L2AGLb")).click();
+
+        //Найти Дашку
+        $(byName("q")).setValue("Dshkaa").pressEnter();
+
+        //Проверить, что "Дашка" появилась в результатах поиска
+        $("html").shouldHave(Condition.text("@dshkaa"));
+
+        //Открыть профиль
+        element(byXpath("//div[@class='byrV5b']")).click();
+
+        sleep(10000);
+    }
+}
